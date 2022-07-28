83ba2fe50b2a541bebe885e4a641a4d50cae8946
Andrea Terzolo <andrea.terzolo@polito.it>
2022-07-28 10:38:01 +0200

    new(modern_bpf): add `fchmod` syscall
    Signed-off-by: Andrea Terzolo <andrea.terzolo@polito.it>

--- userspace/libpman/src/events_prog_names.h
+++ userspace/libpman/src/events_prog_names.h
@@ -39,6 +39,8 @@ static const char* event_prog_names[PPM_EVENT_MAX] = {
 	[PPME_SYSCALL_CHROOT_X] = "chroot_x",
 	[PPME_SYSCALL_FCHDIR_E] = "fchdir_e",
 	[PPME_SYSCALL_FCHDIR_X] = "fchdir_x",
+	[PPME_SYSCALL_FCHMOD_E] = "fchmod_e",
+	[PPME_SYSCALL_FCHMOD_X] = "fchmod_x",
 };
 
 /* Some events can require more than one bpf program to collect all the data. */
