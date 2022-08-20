ce16899030d1bbe346a2ca509d2a969854708792
Andrea Terzolo <andrea.terzolo@polito.it>
2022-08-20 14:55:50 +0000

    new(modern_bpf): add `clone` syscall
    Signed-off-by: Andrea Terzolo <andrea.terzolo@polito.it>

--- driver/modern_bpf/definitions/events_dimensions.h
+++ driver/modern_bpf/definitions/events_dimensions.h
@@ -61,5 +61,6 @@
 #define PTRACE_E_SIZE HEADER_LEN + sizeof(int64_t) + sizeof(uint16_t) + PARAM_LEN * 2
 #define CAPSET_E_SIZE HEADER_LEN
 #define CAPSET_X_SIZE HEADER_LEN + sizeof(int64_t) + sizeof(uint64_t) * 3 + PARAM_LEN * 4
+#define CLONE_E_SIZE HEADER_LEN
 
 #endif /* __EVENT_DIMENSIONS_H__ */
