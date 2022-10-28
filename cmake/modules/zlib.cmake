7de0a44025367e45c7e25fba9014328cacac1256
Andrea Terzolo <andrea.terzolo@polito.it>
2022-10-28 15:49:14 +0000

    temp
    Signed-off-by: Andrea Terzolo <andrea.terzolo@polito.it>

--- cmake/modules/zlib.cmake
+++ cmake/modules/zlib.cmake
@@ -38,8 +38,9 @@ else()
 				PREFIX "${PROJECT_BINARY_DIR}/zlib-prefix"
 				URL "https://github.com/madler/zlib/archive/v1.2.11.tar.gz"
 				URL_HASH "SHA256=629380c90a77b964d896ed37163f5c3a34f6e6d897311f1df2a7016355c45eff"
-				CONFIGURE_COMMAND ./configure --prefix=${ZLIB_SRC} --static
+				CONFIGURE_COMMAND ./configure --prefix=${ZLIB_SRC}
 				BUILD_COMMAND ${CMD_MAKE}
+				COMMAND rm -rf libz.so
 				BUILD_IN_SOURCE 1
 				BUILD_BYPRODUCTS ${ZLIB_LIB}
 				INSTALL_COMMAND "")
